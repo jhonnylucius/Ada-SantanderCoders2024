1b56eb0a63d36551e918a228756eb0cdbeb94c67
bootCampSantander <jhonnyluciusaa@gmail.com>
2024-06-13 11:36:54 -0300

    Exemplologica - Curso Ada - Santander DevOps 2024

---
+++ Exemplologica/src/Exemplologica.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Locale.LanguageRange;
+
+public class Exemplologica {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            String nome;
+
+        System.out.println("Digite seu nome");
+        nome = scanner.next();
+        System.out.println("Bom Dia, " + nome + ".");
+        }
+}
